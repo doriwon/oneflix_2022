5f800a4e8366cbb12b0fc629ef8566fdb2b44c89
sonwon <sonwon@howser.co.kr>
2022-02-01 18:55:49 +0900

    #4.4 Route States

--- src/routers/Coin.tsx
+++ src/routers/Coin.tsx
@@ -1,11 +1,49 @@
-import { useParams } from "react-router";
+import { useLocation, useParams } from "react-router";
+import { useState } from "react";
+import styled from "styled-components";
+
+const Container = styled.div`
+  padding: 0 20px;
+  max-width: 480px;
+  margin: 0 auto;
+`;
+
+const Header = styled.header`
+  height: 10vh;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+`;
+
+const Title = styled.h1`
+  font-size: 30px;
+  color: ${(props) => props.theme.accentColor};
+`;
+
+const Loader = styled.span`
+  display: block;
+  text-align: center;
+`;
 
 interface RouteParams {
   coinId: string;
 }
+interface RouteState {
+  name: string;
+}
 
 function Coin() {
   const { coinId } = useParams<RouteParams>();
-  return <h1>Coin: {coinId}</h1>;
+  const [loading, setLoading] = useState(true);
+  const { state } = useLocation<RouteState>();
+  return (
+    <Container>
+      <Header>
+        <Title>{state?.name || "Loading"}</Title>
+        {/* 상세페이지로 바로 유입될 씨 로딩 문구 노출 (전 페이지에서 name이란 state를 못가져왔으니) */}
+      </Header>
+      {loading ? <Loader>Loading...</Loader> : null}{" "}
+    </Container>
+  );
 }
 export default Coin;
--- src/routers/Coins.tsx
+++ src/routers/Coins.tsx
@@ -28,8 +28,9 @@ const Coin = styled.li`
   border-radius: 15px;
   color: ${(props) => props.theme.backgroundColor};
   a {
-    display: block;
-    padding: 20px;
+    display: flex;
+    align-items: center;
+    padding: 15px 20px;
     transition: color 0.2s ease-in;
   }
   &:hover {
@@ -42,6 +43,12 @@ const Coin = styled.li`
 const Loader = styled.span`
   display: block;
   text-align: center;
+`;
+
+const Img = styled.img`
+  width: 30px;
+  height: 30px;
+  margin-right: 15px;
 `;
 
 interface CoinInterface {
@@ -76,7 +83,17 @@ function Coins() {
         <CoinsList>
           {coins.map((coin) => (
             <Coin key={coin.id}>
-              <Link to={`/${coin.id}`}>{coin.name} &rarr; </Link>
+              <Link
+                to={{
+                  pathname: `/${coin.id}`,
+                  state: { name: coin.name }, //해당 페이지로 name 이란 정보 state로 전송
+                }}
+              >
+                <Img
+                  src={`https://cryptoicon-api.vercel.app/api/icon/${coin.symbol.toLowerCase()}`}
+                />
+                {coin.name} &rarr;{" "}
+              </Link>
             </Coin>
           ))}
         </CoinsList>
