22c46c646f5dfb728a91bd36563e92d7a0a83524
sonwon <sonwon@howser.co.kr>
2022-02-23 22:24:44 +0900

    #5.17 Selectors part 2

--- src/atoms.tsx
+++ src/atoms.tsx
@@ -6,16 +6,15 @@ export interface IToDo {
   category: "TO_DO" | "DOING" | "DONE"; //3개중 하나
 }
 
+export const categoryState = atom({ key: "category", default: "TO_DO" });
+
 export const toDoState = atom<IToDo[]>({ key: "toDo", default: [] });
 
 export const toDoSelector = selector({
   key: "toDoSelector",
   get: ({ get }) => {
     const toDos = get(toDoState);
-    return [
-      toDos.filter((toDo) => toDo.category === "TO_DO"),
-      toDos.filter((toDo) => toDo.category === "DOING"),
-      toDos.filter((toDo) => toDo.category === "DONE"),
-    ]; // [[{},{}]] 형태의 배열을 return
+    const category = get(categoryState);
+    return toDos.filter((toDo) => toDo.category === category);
   },
 });
