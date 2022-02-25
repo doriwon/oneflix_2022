6b59681e67672eb4b1ccf8b9e4cd719e62c2653b
sonwon <sonwon@howser.co.kr>
2022-02-25 23:18:41 +0900

    globalstyle reset

--- src/theme.ts
+++ src/theme.ts
@@ -1,15 +1,7 @@
 import { DefaultTheme } from "styled-components";
 
 export const darkTheme: DefaultTheme = {
-  bgColor: "#2f3640",
-  textColor: "white",
-  accentColor: "#44bd32",
-  cardBgColor: "transparent",
-};
-
-export const lightTheme: DefaultTheme = {
-  bgColor: "whitesmoke",
-  textColor: "#black",
-  accentColor: "#44bd32",
-  cardBgColor: "white",
+  bgColor: "#3F8CF2",
+  boardColor: "#DADFE9",
+  cardColor: "white",
 };
